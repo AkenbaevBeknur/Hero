33c4473bc218d24e3ab98b5997747f5b68b18ba4
Beknur <112559670+AkenbaevBeknur@users.noreply.github.com>
2023-02-24 08:59:58 +0600

    Initial commit2

--- src/System/Hero.java
+++ src/System/Hero.java
@@ -1,2 +1,47 @@
-package System;public class Hero {
+package System;
+
+public class Hero {
+    private String name;
+    private int health;
+    private int damage;
+    private String superpower;
+
+    public Hero(String name,int health, int damage, String superpower) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+        this.superpower = superpower;
+    }
+
+    public Hero(String name,int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public String getSuperpower() {
+        return superpower;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + name + '\'' +
+                ", health=" + health +
+                ", damage=" + damage +
+                ", superpower='" + superpower + '\'' +
+                '}';
+    }
 }
